70d7d8952e9ef7d5b86ad18b21199e9223ebf210
DaniPrr90 <danilo.pirro@libero.it>
2024-10-15 15:50:38 +0200

    add: Modifica ultimata

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ console.log("sono dentro la console")
 /*stampo i valori da 1 a 100 */
 
 for (let i = 1; i < 101; i++) {
-    console.log(i);
+    
 
     if (i % 3 == 0 && i % 5 == 0) {
         console.log("FizzBuzz")
@@ -22,8 +22,21 @@ for (let i = 1; i < 101; i++) {
 
     }
    }
-    
+    /*
+     Se il valore è divisibile di 3 stampo solo Fizz 
+     */
 
+if(i % 3 !=0 && i % 5 !=0){ 
+console.log(i);
+}
+
+     /*
+     se il valore è divisibile di 5 stampo solo buzz
+     */
+
+     /*
+     se il valore e divisibile sia per 3 sia per 5 stampo solo FizzBuzz
+     */
 }
 
 
